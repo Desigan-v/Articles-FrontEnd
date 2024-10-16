8d0aff0c375f303528a36fddfcc928f37ddc9ee0
Desigan V <desigan1506@gmail.com>
2024-10-16 15:48:19 +0530

    Update ArticleList.tsx

--- src/components/ArticleList.tsx
+++ src/components/ArticleList.tsx
@@ -37,7 +37,7 @@ const ArticleList = () => {
             <li key={article.id} className="article-item">
               <h3 className="article-title">{article.article}</h3>
               <p className="article-description">{article.description}</p>
-              <h3 className="article-author">Author: {article.authorName}</h3>
+              <h4 className="article-author">Author: {article.authorName}</h4>
               {article.articleFile && (
                 <a href={`http://localhost:3000/${article.articleFile}`} target="_blank" className="view-article-link">
                   View Article
