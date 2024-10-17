d66a7a7d4f0b04aa9010d435b27ecd0055748c8c
Desigan <desigan@gmail.com>
2024-10-17 11:55:00 +0530

    new file:   public/images/oops.jpeg 	new file:   public/images/pro.jpg 	new file:   public/images/pro1.jpg 	modified:   src/app/globals.css 	modified:   src/app/page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import ArticleList from '../components/ArticleList';
 import AuthorArticles from '../components/AuthorArticles';
 import AllArticles from '../components/AllArticles';
 import "./globals.css";
-
+import Image from 'next/image';
 const Home = () => {
   const [view, setView] = useState(''); // State to handle view rendering
 
@@ -13,8 +13,14 @@ const Home = () => {
 
   return (
     <div className="container">
+            <Image className='img'
+                  src="/images/pro1.jpg"  // Ensure the file extension is correct
+                  alt="Oops"
+                  width={1070}
+                  height={200}
+                  priority
+              />
         <center><h1>Article Management</h1></center>
-
         <div className="button-container">
           <button className="button" onClick={() => handleViewChange('add')}>
             Add Article
@@ -30,49 +36,15 @@ const Home = () => {
           </button>
         </div>
 
-        
         <div className="content">
           {view === 'add' && <AddArticle />}
           {view === 'findByName' && <ArticleList />}
           {view === 'findByAuthor' && <AuthorArticles />}
-          {view === 'viewAll' && <AllArticles />}
+          {view === 'viewAll' && <AllArticles />}<br></br>
+          <center><h4>Welcome To Articles World</h4></center>
         </div>
     </div>
   );
 };
 
 export default Home;
-
-
-/*import React, { useState } from 'react';
-import AddArticle from '../components/AddArticle';
-import ArticleList from '../components/ArticleList';
-import AuthorArticles from '../components/AuthorArticles';
-import AllArticles from '../components/AllArticles';
-import "./globals.css";
-
-const Home = () => {
-  const [view, setView] = useState(''); // State to handle view rendering
-
-  const handleViewChange = (newView: string) => setView(newView);
-
-  return (
-    <div>
-      <center>
-      <h1>Article Management</h1>
-      <button onClick={() => handleViewChange('add')}>Add Article</button>
-      <button onClick={() => handleViewChange('findByName')}>Find by Article Name</button>
-      <button onClick={() => handleViewChange('findByAuthor')}>Find by Author Name</button>
-      <button onClick={() => handleViewChange('viewAll')}>View All Articles</button>
-
-      {view === 'add' && <AddArticle />}
-      {view === 'findByName' && <ArticleList />}
-      {view === 'findByAuthor' && <AuthorArticles />}
-      {view === 'viewAll' && <AllArticles />}
-      </center>
-    </div>
-  );
-};
-
-export default Home;
-*/
