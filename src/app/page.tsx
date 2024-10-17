2d85cc2cf8c9d32cfde62cd7622b1af7b2152ab8
Desigan <desigan@gmail.com>
2024-10-17 12:15:43 +0530

    modified:   src/app/globals.css modified:   src/app/page.tsx add:        public/images/.jpg files

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -16,7 +16,7 @@ const Home = () => {
             <Image className='img'
                   src="/images/pro1.jpg"  // Ensure the file extension is correct
                   alt="Oops"
-                  width={1070}
+                  width={838}
                   height={200}
                   priority
               />
@@ -48,37 +48,3 @@ const Home = () => {
 };
 
 export default Home;
-
-
-/*import React, { useState } from 'react';
-import AddArticle from '../components/AddArticle';
-import ArticleList from '../components/ArticleList';
-import AuthorArticles from '../components/AuthorArticles';
-import AllArticles from '../components/AllArticles';
-import "./globals.css";
-
-const Home = () => {
-  const [view, setView] = useState(''); // State to handle view rendering
-
-  const handleViewChange = (newView: string) => setView(newView);
-
-  return (
-    <div>
-      <center>
-      <h1>Article Management</h1>
-      <button onClick={() => handleViewChange('add')}>Add Article</button>
-      <button onClick={() => handleViewChange('findByName')}>Find by Article Name</button>
-      <button onClick={() => handleViewChange('findByAuthor')}>Find by Author Name</button>
-      <button onClick={() => handleViewChange('viewAll')}>View All Articles</button>
-
-      {view === 'add' && <AddArticle />}
-      {view === 'findByName' && <ArticleList />}
-      {view === 'findByAuthor' && <AuthorArticles />}
-      {view === 'viewAll' && <AllArticles />}
-      </center>
-    </div>
-  );
-};
-
-export default Home;
-*/
